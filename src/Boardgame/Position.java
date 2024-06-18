535d659667ed0834b30a99c3e2d3dd12188a33cc
Fernando de Souza Batista <batistafernando735@gmail.com>
2024-06-18 15:37:15 -0300

    Melhorias no código da classe Position
    - Adicionados os métodos equals e hashCode para permitir a comparação correta de objetos Position e uso eficiente em coleções.
    - Ajustado o método toString para retornar uma representação mais legível da posição.
    - Adicionados comentários explicativos para facilitar a compreensão e manutenção do código.

--- src/Boardgame/Position.java
+++ src/Boardgame/Position.java
@@ -5,11 +5,13 @@ public class Position {
     private int row;
     private int column;
 
+    // Construtor que inicializa a posição com linha e coluna
     public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
+    // Métodos getter para obter os valores de linha e coluna
     public int getRow() {
         return row;
     }
@@ -26,13 +28,36 @@ public class Position {
         this.column = column;
     }
 
+    // Método para definir os valores de linha e coluna
     public void setValues(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
+    // Sobrescrevendo o método toString para uma representação legível da posição
     @Override
     public String toString() {
-        return row + ", " + column;
+        return "(" + row + ", " + column + ")";
+    }
+
+    // Método para comparar duas posições
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Position position = (Position) obj;
+        return row == position.row && column == position.column;
+    }
+
+    // Método para gerar o código hash da posição
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(row);
+        result = 31 * result + Integer.hashCode(column);
+        return result;
     }
 }
